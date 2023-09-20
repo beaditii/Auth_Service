3f57aa54622bfac3705f754b2c79adee03351302
Aditi Kumari <aditisingh49893@gmail.com>
2023-09-20 22:37:36 +0530

    Added jwt create and verify the token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository=require('../repository/user-repository');
-
+const jwt=require('jsonwebtoken');
+const { JWT_KEY } = require('../config/serverConfig');
 class UserService{
     constructor(){
         this.userRepository=new UserRepository();
@@ -13,6 +14,29 @@ class UserService{
         console.log("something went wrong in the service layer");
         throw error;
        }
+    }
+
+     createToken(user){
+        try{
+          const result=jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+          return result;
+        }
+        catch(error){
+            console.log("something went wrong in token creation");
+        throw error;
+        }
+    }
+
+    verifyToken(token){
+        try{
+         const response=jwt.verify(token,JWT_KEY);
+         return response;
+        }
+        catch(error){
+            console.log("something went wrong in token validation");
+        throw error;
+        }
+
     }
 }
 
